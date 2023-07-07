cfb7336b5303b902bd21788e1a7115e4ccf56833
yeonwoo shin <tlsxkrqhd@naver.com>
2023-07-07 23:04:32 +0900

    realLast

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createMemoryRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./Pages/Home";
 import Sale from "./Pages/Sale";
 import Signup from "./Pages/Signup";
 
-export const PageRouter = createBrowserRouter([
+export const PageRouter = createMemoryRouter([
   {
     path: "/",
     element: <App />,
